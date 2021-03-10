81760468d802cd6c07a1723e5e8edb46840edd65
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-03-10 12:21:20 -0500

    fix noreferrer error

--- client/src/components/Shared/Footer/Footer.jsx
+++ client/src/components/Shared/Footer/Footer.jsx
@@ -3,28 +3,28 @@ import './Footer.css'
 const Footer = () => (
   <footer>
     <div className="hutch-footer">
-    <a href="https://github.com/hutchskistoke" target="_blank">
+    <a href="https://github.com/hutchskistoke" rel="noreferrer" target="_blank">
         <img src="https://www.iconsdb.com/icons/preview/white/github-11-xxl.png" alt="github" className="footer-photo-git" />
     </a>
       <p className="member-name">Ashley Hutchinson</p>
     </div>
 
     <div className="chandler-footer">
-    <a href="https://github.com/Chandler-Manly" target="_blank">
+    <a href="https://github.com/Chandler-Manly" rel="noreferrer" target="_blank">
         <img src="https://www.iconsdb.com/icons/preview/white/github-11-xxl.png" alt="github" className="footer-photo-git" />
     </a>
       <p className="member-name">Chandler Manly</p>
     </div>
 
     <div className="shamma-footer">
-    <a href="https://github.com/snqureshi" target="_blank">
+    <a href="https://github.com/snqureshi" rel="noreferrer" target="_blank">
         <img src="https://www.iconsdb.com/icons/preview/white/github-11-xxl.png" alt="github" className="footer-photo-git" />
     </a>
       <p className="member-name">Shamma Qureshi</p>
     </div>
 
     <div className="jess-footer">
-    <a href="https://github.com/jessicamyee" target="_blank">
+    <a href="https://github.com/jessicamyee" rel="noreferrer" target="_blank">
         <img src="https://www.iconsdb.com/icons/preview/white/github-11-xxl.png" alt="github" className="footer-photo-git" />
     </a>
       <p className="member-name">Jessica Yee</p>
