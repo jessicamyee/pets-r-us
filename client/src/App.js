3cd9bc2fb0952e2d98883ecf711e31781bca65a6
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-03-09 21:39:35 -0500

    add to App.js and ensure all routes in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,61 @@
+import React, { useState, useEffect } from "react";
+import Home from "./screens/Home/Home";
+// import AllPets from "./screens/AllPets/AllPets";
+import CreatePet from "./screens/CreatePet/CreatePet";
+import EditPet from "./screens/EditPet/EditPet";
+import PetDetail from "./screens/PetDetails/PetDetail";
+import SignUp from "./screens/SignUp/SignUp";
+import SignIn from "./screens/SignIn/SignIn";
+import SignOut from "./screens/SignOut/SignOut";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { verifyUser } from "./services/users";
+// import './App.css'
 
-import './App.css';
+const App = () => {
+  const [user, setUser] = useState(null);
 
-function App() {
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
+
+  const clearUser = () => setUser(null);
+
+  console.log(user);
+
   return (
-    <div className="App">
-     <h1>Hi there!</h1>
+    <div className="app-section">
+      <Switch>
+        <Route exact path="/">
+          <Home user={user} />
+        </Route>
+        <Route path="/sign-up">
+          <SignUp setUser={setUser} />
+        </Route>
+        <Route path="/sign-in">
+          <SignIn setUser={setUser} />
+        </Route>
+        <Route path="/sign-out">
+          <SignOut setUser={setUser} clearUser={clearUser} />
+        </Route>
+        {/* <Route exact path="/pets">
+          <AllPets user={user} />
+        </Route> */}
+        <Route path="/add-pet">
+          {user ? <CreatePet user={user} /> : <Redirect to="/sign-up" />}
+        </Route>
+        <Route exact path="/pets/:id/edit">
+          {user ? <EditPet user={user} /> : <Redirect to='/' />}
+        </Route>
+        <Route exact path="/pets/:id">
+          <PetDetail user={user} />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Layout from "../../components/Shared/Layout/Layout"
+
+import React from 'react';
+
+function Home(props) {
+  return (
+    <div>
+      <Layout user={props.user} />
+    </div>
+  );
+}
+
+export default Home;
