f31fa2b4460430f95a8c1a3111fffd0ae2d96039
Ashley Hutchinson <hutchski@Ashleys-MacBook-Pro.local>
2021-03-10 09:30:34 -0500

    resolving merge conflict in home

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,15 @@
+
+import Layout from "../../components/shared/Layout/Layout"
+// import "./Home.css"
+
+const Home = () => {
+  return (
+    <Layout>
+      <div>
+        This is home
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
