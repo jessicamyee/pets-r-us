7dc7c56f67f9736767e6b1eff91fa5ffdb3a91af
Jessica Yee <jessicayee@Jessicas-MacBook-Pro.local>
2021-03-10 23:36:50 -0500

    remove un-needed comments from nav.jsx

--- client/src/components/Shared/Nav/Nav.jsx
+++ client/src/components/Shared/Nav/Nav.jsx
@@ -1,10 +1,10 @@
-import './Nav.css'
+import "./Nav.css";
 import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 const authenticatedOptions = (
   <>
-    <Nav.Link as={ NavLink } to="/add-pet" className="nav-tabs" >
+    <Nav.Link as={NavLink} to="/add-pet" className="nav-tabs">
       Add a Pet
     </Nav.Link>
     <Nav.Link className="nav-tabs" href="/sign-out">
@@ -34,54 +34,19 @@ const alwaysOptions = (
 
 const MainNav = ({ user }) => {
   return (
-    <Navbar className="nav-section"  expand="lg" >
-      <Navbar.Brand href="/" className="page-logo">Pets-R-Us</Navbar.Brand>
+    <Navbar className="nav-section" expand="lg">
+      <Navbar.Brand href="/" className="page-logo">
+        Pets-R-Us
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggle" />
-      <Navbar.Collapse id="basic-navbar-nav" >
+      <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          {/* <Nav.Link> */}
-
           {user && <div className="welcome-msg">Welcome, {user.username}</div>}
           {alwaysOptions}
           {user ? authenticatedOptions : unauthenticatedOptions}
-
-          {/* </Nav.Link> */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 };
 export default MainNav;
-
-// const MainNav = ({ user }) => {
-//   return (
-//     <Navbar bg="light" expand="lg">
-//       <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//       <Navbar.Collapse id="basic-navbar-nav">
-//         <Nav className="mr-auto">
-//           <Nav.Link href="#home">Home</Nav.Link>
-//           <Nav.Link href="#link">Link</Nav.Link>
-//         </Nav>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   );
-// };
-// export default MainNav;
-
-//     <nav>
-//       <div className="nav">
-//         <NavLink className="logo" to="/">
-//           Pets-R-Us
-//         </NavLink>
-//         <div className="links">
-//           {user && <div className="link welcome">Welcome, {user.username}</div>}
-//           {alwaysOptions}
-//           {user ? authenticatedOptions : unauthenticatedOptions}
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Nav;
