fba7bec98ca8444ea0686227f8ffe5937e1bf3bc
HC <manlychandler@gmail.com>
2021-03-12 11:43:55 -0500

    added margin to form fields

--- client/src/screens/CreatePet/Step1.jsx
+++ client/src/screens/CreatePet/Step1.jsx
@@ -45,7 +45,6 @@ function Step1(props) {
         <option value="" disable selected>
           Select Gender
         </option>
-
         <option value="male">Male</option>
         <option value="female">Female</option>
         <option value="other">Other</option>
