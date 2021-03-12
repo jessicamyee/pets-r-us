ea14cecd171c5c29f28fd3f553193da84e430af3
Ashley Hutchinson <hutchski@Ashleys-MacBook-Pro.local>
2021-03-12 12:03:53 -0500

    hover effect on pet card and links for carousel

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -2,7 +2,7 @@ import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } fro
 import React from "react";
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import './Carousel.css'
-
+import {Link} from "react-router-dom"
 
 export default class Carousel extends React.Component {
 
@@ -15,21 +15,28 @@ export default class Carousel extends React.Component {
         naturalSlideHeight={125}
         totalSlides={3}
       >
+
         <Slider>
           <Slide index={0}>
+            <Link to="/pets/604a5ce5c01a0e290be50d11">
             <img
               className="carousel-image"
               src="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50708836/1/?bust=1615398914&width=1080"
               alt="animal" />
+          </Link>
           </Slide>
           <Slide index={1}>
-            <img className="carousel-image" src="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50829869/2/?bust=1615479317&width=1080" alt="animal" />
+            <Link to="/pets/604a5ce5c01a0e290be50d12">
+              <img className="carousel-image" src="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/50829869/2/?bust=1615479317&width=1080" alt="animal" />
+            </Link>
           </Slide>
           <Slide index={2}>
-            <img className="carousel-image" src="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/48885469/1/?bust=1614728729&width=720" alt="animal" />
+            <Link to="/pets/604a5ce5c01a0e290be50d14">
+              <img className="carousel-image" src="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/48885469/1/?bust=1614728729&width=720" alt="animal" />
+            </Link>
           </Slide>
-        </Slider>
-
+          </Slider>
+          
         <ButtonBack>&#171;</ButtonBack>
         <ButtonNext>&#187;</ButtonNext>
         {/* <DotGroup></DotGroup> */}
