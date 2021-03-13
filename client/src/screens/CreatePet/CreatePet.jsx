35ce1ee38d9d959b1238c50bc99b094c5872bc78
HC <manlychandler@gmail.com>
2021-03-13 14:21:55 -0500

    added brand statement to form page with ipsum language

--- client/src/screens/CreatePet/CreatePet.jsx
+++ client/src/screens/CreatePet/CreatePet.jsx
@@ -22,6 +22,7 @@ const CreatePet = (props) => {
     <Layout user={props.user}>
       <Parent />
       <BackgroundVideo />
+      <div className="brand-statement">Ipsum est ad anim ad commodo. Et dolore do reprehenderit dolore. Labore laborum do non sit labore deserunt ipsum labore excepteur id velit. Ex sit ea irure dolore laborum eiusmod adipisicing duis excepteur et. Sit excepteur veniam commodo ipsum duis exercitation quis. Est tempor exercitation ex est anim magna ut veniam consequat. Aute ex elit culpa aute cupidatat anim do est laborum laborum laboris aliquip laborum.</div>
       <img className="form-images" src="https://images.unsplash.com/photo-1560967560-2a19f2ca05b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2852&q=80" />
       <img className="form-images" src="https://images.unsplash.com/photo-1601758124360-86f5a0432c42?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8cGV0JTIwb3duZXJzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" />
       <img className="form-images" src="https://images.unsplash.com/photo-1592951117908-4acda89ee817?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fHBldCUyMG93bmVyc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" />
