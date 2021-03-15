18bf1f97f58bdd7883cbacec98fa94cf845b57d6
HC <manlychandler@gmail.com>
2021-03-15 16:02:52 -0400

    updated video file to include cats

--- client/src/screens/CreatePet/BackgroundVideo.jsx
+++ client/src/screens/CreatePet/BackgroundVideo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./BackgroundVideo.css"
 
 const BackgroundVideo = () => {
-  const videoSource = "https://res.cloudinary.com/dygk00sc0/video/upload/v1615651527/pets-r-us_f4qrcu.mp4"
+  const videoSource = "https://res.cloudinary.com/dygk00sc0/video/upload/v1615838147/pets-r-us_2.0_hwksf3.mp4"
 
   return (
     <div className="background-video">
